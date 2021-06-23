998201b9f64421a7f58acb2a9d5a49b59aab632e
Yazid Taki <46725845+Toucano@users.noreply.github.com>
2021-06-23 19:58:28 +0100

    First criteria of the project: The view should get the content of the encyclopedia entry by calling the appropriate util function.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,9 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    return render(request, "encyclopedia/entry.html", {
+        "entrytitle": entry.capitalize(),
+        "entry": util.get_entry(entry)
+    })
+
